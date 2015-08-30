da0d11a2a9f7eeb1422e859d3d778bea0c0f9f7c
BuildTools <unconfigured@null.spigotmc.org>
2015-08-29 21:23:51 -0300

    index

---
+++ index.php
@@ -0,0 +1,287 @@
+<?php require_once 'classes/DB.class.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <meta http-equiv="Cache-control" content="public"> -->
+        <link rel="icon" href="img/logo_16x16.png" type="image/x-icon" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>IF - Pictures</title>
+        <!-- Bootstrap and Bootstrap Flat -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-flat.min.css" rel="stylesheet">
+        <link href="css/bootstrap-flat-extras.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Sintony' rel='stylesheet' type='text/css'>
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/animate.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body id="page-top">
+        <section class="header">
+            <div class="text-center">
+                <h2><a href="#"><font class="if-logo">IF - </font><font class="pictures-logo">PICTURES</font></a></h2>
+                <h4>Bem vindos ao IF - Pictures</h4>
+            </div>
+        </section>
+		<nav class="navbar navbar-default" data-spy="affix" data-offset-top="400" id="nav">
+		  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+		        <span class="sr-only">Mudar navegação</span>
+		        <span class="fa fa-bars fa-2x"></span>
+		      </button>
+		      <a class="page-scroll" href="#page-top"><img class="lazy navbar-brand" data-original="img/logo.png" alt="IF - Logo"></a>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="navbar-collapse">
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Link</a></li>
+		        <li class="active"><a href="#">Link</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+        <header>
+            <div class="header-content">
+                <div class="header-content-inner wow fadeIn">
+                    <h1>Temos o melhor site para compartilhar suas imagens!</h1>
+                    <hr class="secondary">
+                    <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> -->
+                </div>
+            </div>
+        </header>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <!-- <div class="col-lg-8 col-lg-offset-2 text-center"> -->
+                    <div class="text-center wow fadeInDown">
+                        <a href="#" class="btn btn-primary btn-xl"><span class="fa fa-check-square-o"></span> Cadastro</a> <a href="#" class="btn btn-success btn-xl"><span class="fa fa-user"></span> Entrar</a>
+                    </div>
+                    <div class="divider">
+                        <div class="box">
+                            <span class="fa fa-angle-down"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-1">
+            <div class="container">
+                <div class="row">
+                    <div class="text-center wow fadeIn">
+                        <h1>Desenvolvido com carinho!</h1>
+                        <hr class="primary">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <br>
+                        <img class="lazy img-responsive wow fadeInLeft" data-original="img/nexus4_portrait.png" width="170" alt="nexus4_portrait">
+                        <img class="lazy img-responsive wow fadeInRight" data-original="img/css3-html5-bootstrap.png" width="625" alt="css3-html5-bootstrap">
+                    </div>
+                     <div class="divider">
+                        <div class="box">
+                            <span class="fa fa-angle-down"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-2">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="container">
+	            		<div class="row text-center wow fadeIn">
+	            			<!-- <h3>VEJA O QUE TEMOS A OFERECER</h3>
+	            			<p>Website responsivo para compartilhamento de imagens</p> -->
+	            			<br>
+	            		</div>
+	            	</div>
+	                <div class="col-lg-3 col-md-6 col-sm-6 text-center">
+	                    <div class="service-box">
+	                        <i class="fa fa-5x fa-desktop hover-size wow bounceIn"></i>
+	                    	<h3>DESIGN RESPONSIVO</h3>
+	                    </div>
+	                    <p>Nosso website foi desenvolvido para qualquer dispositivo.</p>
+	                </div>
+	                <div class="col-lg-3 col-md-6 col-sm-6 text-center">
+	                    <div class="service-box">
+	                        <i class="fa fa-5x fa-lightbulb-o hover-size wow bounceIn"></i>
+	                        <h3>INOVADOR</h3>
+	                    </div>
+	                    <p>Novas idéias para inovar o conceito do compartilhamento de imagens.</p>
+	                </div>
+	                <div class="col-lg-3 col-md-6 col-sm-6 text-center">
+	                    <div class="service-box">
+	                        <i class="fa fa-5x fa-picture-o hover-size wow bounceIn"></i>
+	                        <h3>COMPARTILHE</h3>
+	                    </div>
+	                    <p>Faça upload e compartilhe suas imagens de forma eficiente.</p>
+	                </div>
+	                <div class="col-lg-3 col-md-6 col-sm-6 text-center">
+	                    <div class="service-box">
+	                        <i class="fa fa-5x fa-rocket hover-size wow bounceIn"></i>
+	                        <h3>ÁGIL</h3>
+	                    </div>
+	                    <p>Sistema totalmente otimizado para uma melhor experiência.</p>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="divider">
+                <div class="box">
+                    <span class="fa fa-angle-down"></span>
+                </div>
+            </div>
+        </section>
+        <section class="section-3">
+        	<div class="container">
+        		<div class="row">
+					<div class="col-md-12 wow fadeIn" data-wow-delay="0.2s">
+                        <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+                            <!-- Bottom Carousel Indicators -->
+                            <ol class="carousel-indicators">
+                                <li data-target="#quote-carousel" data-slide-to="0" class="active"><img class="img-responsive " src="https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg" alt="">
+                                </li>
+                                <li data-target="#quote-carousel" data-slide-to="1"><img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg" alt="">
+                                </li>
+                                <li data-target="#quote-carousel" data-slide-to="2"><img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" alt="">
+                                </li>
+                            </ol>
+
+                            <!-- Carousel Slides / Quotes -->
+                            <div class="carousel-inner text-center">
+
+                                <!-- Quote 1 -->
+                                <div class="item active">
+                                    <blockquote>
+                                        <div class="row">
+                                            <div class="col-sm-8 col-sm-offset-2">
+
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+                                                <small>Someone famous</small>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- Quote 2 -->
+                                <div class="item">
+                                    <blockquote>
+                                        <div class="row">
+                                            <div class="col-sm-8 col-sm-offset-2">
+
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                                                <small>Someone famous</small>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- Quote 3 -->
+                                <div class="item">
+                                    <blockquote>
+                                        <div class="row">
+                                            <div class="col-sm-8 col-sm-offset-2">
+
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. .</p>
+                                                <small>Someone famous</small>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                            </div>
+
+                            <!-- Carousel Buttons Next/Prev -->
+                            <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
+                            <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+        		</div>
+        	</div>
+        </section>
+        <section class="section-4">
+        	<div class="container">
+        		<div class="row">
+        			<div class="col-lg-4 col-sm-4 wow fadeInDown">
+        				<div class="boxed-content left-aligned left-boxed-icon">
+				      		<span class="icon fa fa-quote-left"></span>
+				      		<h2 class="spacer-bottom boxed-content-title">Sobre</h2>
+	                    	<p class="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+				    	</div>
+	                </div>
+	                <div class="col-lg-4 col-sm-4 wow fadeInUp">
+        				<div class="boxed-content left-aligned left-boxed-icon">
+				      		<span class="icon fa fa-link"></span>
+				      		<h2 class="spacer-bottom boxed-content-title">Menu</h2>
+		                    <ul class="box-links">
+		                    	<li><a href="">Link</a></li>
+		                    	<li><a href="">Link</a></li>
+		                    	<li><a href="">Link</a></li>
+		                    	<li><a href="">Link</a></li>
+		                    </ul>
+				    	</div>
+	                </div>
+	                <div class="col-lg-4 col-sm-4 wow fadeInDown">
+        				<div class="boxed-content left-aligned left-boxed-icon">
+				      		<span class="icon fa fa-search"></span>
+				      		<h2 class="spacer-bottom boxed-content-title">Buscar</h2>
+				      		<p>Faça uma pesquisa rápida e encontre o conteúdo que procura em nosso site.</p>
+		                   	<div class="input-group">
+							  	<input type="text" class="form-control" placeholder="Procurar por...">
+								<span class="input-group-btn">
+							    	<button class="btn btn-default" type="button">Encontrar!</button>
+							  	</span>
+							</div><!-- /input-group -->
+				    	</div>
+	                </div>
+        		</div>
+        	</div>
+        </section>
+        <footer>
+			<div class="container">
+				<div class="row">
+					<p>© 2015 IF - Pictures. All Rights Reserved.
+					<a class="pull-right page-scroll" href="#page-top" data-toggle="tooltip" title="Voltar ao topo!"><i class="fa fa-2x fa-angle-up"></i></a>
+					</p>
+				</div>
+			</div>
+        </footer>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/jquery.lazyload.min.js" type="text/javascript"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery.easing.min.js" type="text/javascript"></script>
+        <script src="js/scrolling-nav.js" type="text/javascript"></script>
+        <script src="js/wow.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            new WOW().init();
+        </script>
+        <script type="text/javascript">
+        	$("img.lazy").lazyload({
+			    effect : "fadeIn"
+			});
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#nav').affix({
+                    offset: {
+                        //top: $('header').height()
+                        top: 173
+                    }
+                });
+            });
+        </script>
+        <script>
+        	$(function () {
+				$('[data-toggle="tooltip"]').tooltip()
+			})
+        </script>
+    </body>
+</html>

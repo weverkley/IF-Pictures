76fa819dc940302f7c7cd47a0338cce2812893f2
unknown <gylmax.micrgit config --global user.email gylmaxmicrocell@gmail.comgit config --global user.email gylmax.micr>
2015-10-26 02:37:28 -0200

    Validação Registro

--- public/php/registro.php
+++ public/php/registro.php
@@ -1,4 +1,29 @@
 <?php
-require_once realpath(dirname(__FILE__).'/../inc/global.inc.php');
-$date = new MongoDate();
+require_once '../../inc/global.inc.php';
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$borndate = $_POST['born-date'];
+$sex = $_POST['sex'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$rpassword = $_POST['rpassword'];
+$email = $_POST['email'];
+$question = $_POST['question'];
+$ansewr = $_POST['ansewr'];
+
+$user = new User();
+$data = array(
+	'name' => $name,
+	'surname' => $surname,
+	'borndate' => $borndate,
+	'sex' => $sex,
+	'login' => $login,
+	'password' => md5($password),
+	'email' => $email,
+	'question' => $question,   
+	'ansewr' => $ansewr
+);
+$user->Insert($data);
+
 ?>

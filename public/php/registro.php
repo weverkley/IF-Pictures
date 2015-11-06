805b68a82d85ac571c8ba6f3c4fd687cfd02c7bd
unknown <gylmaxmicrocell@gmail.com>
2015-11-06 00:51:30 -0200

    Validação de login e registro

--- public/php/registro.php
+++ public/php/registro.php
@@ -1,41 +1,54 @@
 <?php
 require_once '../../inc/global.inc.php';
-
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$borndate = $_POST['born-date'];
-$sex = $_POST['sex'];
-$login = $_POST['login'];
-$password = $_POST['password'];
-$rpassword = $_POST['rpassword'];
-$email = $_POST['email'];
-$question = $_POST['question'];
-$answer = $_POST['answer'];
-
+$data = 0;
 $user = new User();
-$data = array(
-	'name' => $name,
-	'surname' => $surname,
-	'borndate' => $borndate,
-	'sex' => $sex,
-	'login' => $login,
-	'password' => md5($password),
-	'email' => $email,
-	'question' => $question,
-	'answer' => $answer,
-	//----//
-	'avatar' => null,
-	'cover' => null,
-	'thoughts' => array(),
-	'job' => null,
-	'state' => null,
-	'city' => null,
-);
-if($user->Insert($data)){
-	echo json_encode("1");
+if ($_POST['email']) {
+	$where = array('email' => $_POST['email']);
+	if($user->SelectOne($where)){
+		$data = 1; // email existe
+	}
 }
-else {
-	echo json_encode("2");
+if ($_POST['login']) {
+	$where = array('login' => $_POST['login']);
+	if($user->SelectOne($where)){
+		$data = 2; // login existe
+	}
 }
+
+if ($data == 0) {
+	$name = $_POST['name'];
+	$surname = $_POST['surname'];
+	$borndate = $_POST['born'];
+	$sex = $_POST['sex'];
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$rpassword = $_POST['rpassword'];
+	$email = $_POST['email'];
+	$question = $_POST['question'];
+	$answer = $_POST['answer'];
+
+	$user = new User();
+	$data = array(
+		'name' => $name,
+		'surname' => $surname,
+		'borndate' => $borndate,
+		'sex' => $sex,
+		'login' => $login,
+		'password' => md5($password),
+		'email' => $email,
+		'question' => $question,
+		'answer' => $answer,
+		//----//
+		'avatar' => null,
+		'cover' => null,
+		'thoughts' => array(),
+		'job' => null,
+		'state' => null,
+		'city' => null,
+	);
+	if($user->Insert($data)){
+		echo 3;
+	}
+} else echo $data;
 
 ?>

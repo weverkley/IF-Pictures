8a55947fc22b56cda316f7a1a7f000e8460cc1c5
weverkley <wever-kley@live.com>
2015-11-06 09:11:36 -0200

    no message

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,4 @@
- //jQuery to collapse the navbar on scroll
+//jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
 	if (document.getElementById("#nav")) {
 	    if ($(".navbar").offset().top > 50) {
@@ -135,4 +135,50 @@ function logout(){
 $( "#logout" ).click(function() {
   	//alert( "Handler for .click() called." );
   	logout();
+});
+
+/*============= About page ==============*/
+$(".about-tab-menu .list-group-item").click(function(e) {
+    e.preventDefault();
+    $(this).siblings('a.active').removeClass("active");
+    $(this).addClass("active");
+    var index = $(this).index();
+    $("div.about-tab>div.about-tab-content").removeClass("active");
+    $("div.about-tab>div.about-tab-content").eq(index).addClass("active");
+});
+
+// EDIÇÃO DENTRO DO SOBRE
+
+$(document).ready(function(){
+	$(".editlink").on("click", function(e){
+	  e.preventDefault();
+		var dataset = $(this).prev(".datainfo");
+		var savebtn = $(this).next(".savebtn");
+		var theid   = dataset.attr("id");
+		var newid   = theid+"-form";
+		var currval = dataset.text();
+		
+		dataset.empty();
+		
+		$('<input type="text" name="'+newid+'" id="'+newid+'" value="'+currval+'" class="input-edit">').appendTo(dataset);
+		
+		$(this).css("display", "none");
+		savebtn.css("display", "block");
+	});
+	$(".savebtn").on("click", function(e){
+		e.preventDefault();
+		var elink   = $(this).prev(".editlink");
+		var dataset = elink.prev(".datainfo");
+		var newid   = dataset.attr("id");
+		
+		var cinput  = "#"+newid+"-form";
+		var einput  = $(cinput);
+		var newval  = einput.attr("value");
+		
+		$(this).css("display", "none");
+		einput.remove();
+		dataset.html(newval);
+		
+		elink.css("display", "block");
+	});
 });
